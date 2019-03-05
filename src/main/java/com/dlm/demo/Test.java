6afecf183bacb121e7c55c85048a5ca8010295a8
brightdlm100 <1323862811@qq.com>
2019-03-05 21:42:06 +0800

    123

--- src/main/java/com/dlm/demo/Test.java
+++ src/main/java/com/dlm/demo/Test.java
@@ -4,48 +4,19 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class Test {
 
 	public static void main(String[] args) {
-		/*
-		 * StudentsUnsatisfiedConditionsDto dto=new
-		 * StudentsUnsatisfiedConditionsDto();
-		 * //System.out.println(JSON.toJSONString(getFiledName(dto))); String []
-		 * fields=getFiledName(dto); for (String string : fields) {
-		 * System.out.println("private Boolean is"+string.substring(0,1).
-		 * toUpperCase()+string.substring(1)+"Null;"); }
-		 */
-		// System.out.println( JSON.toJSONString(getFiledsInfo(new people())));
-		// FreshmanForConfirmDto dto=new FreshmanForConfirmDto();
-		String[] fields = getFiledName(new Object());
-		for (String string : fields) {
-			// String td="<td title="+"\"${"+string+"}\">{{if
-			// "+string+"}}√{{else}}{{/if}}</td>";
-			String td2 = string;
-			// String s = " <td class='width04' title=\"${" + td2 + "}\">${" +
-			// td2 + "}</td>";
-			String s = "		<td  class='width90' title=\"\"></td>";
-			System.out.println(s);
+
+		try {
+			a();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 
-
-		// System.out.println(String.format("%tc", new Date()));
-		// System.out.println(new Date());
-		/*
-		 * String jsString=
-		 * "{\"tableType\":2,\"grade\":\"2021\",\"queryParam\":{\"code\":0,\"msg\":\"success\",\"data\":[{\"settingId\":\"6f92eb0a637541d6a57ac9ce25060527\",\"settingType\":1,\"fieldName\":\"grade\",\"displayName\":\"年级\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":3},{\"settingId\":\"d86d7151d933497e978c82f8ddcdf54f\",\"settingType\":1,\"fieldName\":\"candidateNo\",\"displayName\":\"考生号\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":4},{\"settingId\":\"83fc0f7ceee04ddf892121227dbdfb84\",\"settingType\":1,\"fieldName\":\"nationCode\",\"displayName\":\"民族\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":5},{\"settingId\":\"c22b445e52c447a7925df93359b89825\",\"settingType\":1,\"fieldName\":\"trainingLevel\",\"displayName\":\"培养层次\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":6},{\"settingId\":\"e40d54f9e2574e05a4a4e227f1aa7f30\",\"settingType\":1,\"fieldName\":\"entranceDate\",\"displayName\":\"入学日期\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":7},{\"settingId\":\"b181902dfd934b6aaffccd3131c1c908\",\"settingType\":1,\"fieldName\":\"idCardTypeCode\",\"displayName\":\"证件类型\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":8},{\"settingId\":\"0edb5b3dd19942b9b30a0ab684fe279b\",\"settingType\":1,\"fieldName\":\"idCard\",\"displayName\":\"证件号\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":9},{\"settingId\":\"d65865e5ec1d4e34b0976924c8d72dd6\",\"settingType\":1,\"fieldName\":\"trainingWayCode\",\"displayName\":\"办学形式\",\"isEnabled\":0,\"isTolerateNull\":1,\"orderNO\":1},{\"settingId\":\"d4f6e60832a247e0a77cafb90881e1e0\",\"settingType\":1,\"fieldName\":\"birthday\",\"displayName\":\"出生日期\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":2},{\"settingId\":\"b321a0c5859c48c389f3cf9b128bcfe5\",\"settingType\":1,\"fieldName\":\"sexCode\",\"displayName\":\"性别\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":10},{\"settingId\":\"a925643ef7b94d74a700e1791bca157a\",\"settingType\":1,\"fieldName\":\"studentName\",\"displayName\":\"姓名\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":11},{\"settingId\":\"1976e4a3c1cd406f9afcc4a04ca2b20f\",\"settingType\":1,\"fieldName\":\"studentNo\",\"displayName\":\"学号\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":12},{\"settingId\":\"1e2c7d1bca2847da9fd8fa7e74228618\",\"settingType\":1,\"fieldName\":\"studyFormCode\",\"displayName\":\"学习形式\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":13},{\"settingId\":\"a2a82275a25844cda51479f65375e027\",\"settingType\":1,\"fieldName\":\"educationSystem\",\"displayName\":\"学制\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":14},{\"settingId\":\"c57d40d5ef064746b9c5d00a1bc2faa0\",\"settingType\":1,\"fieldName\":\"PoliticalStatusCode\",\"displayName\":\"政治面貌\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":15},{\"settingId\":\"7fb77c3f4b5c4afbaeb63a949b03ab1b\",\"settingType\":1,\"fieldName\":\"schoolType\",\"displayName\":\"办学类型\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":16},{\"settingId\":\"cc9aebf7a2114125ab8af81afb908e87\",\"settingType\":1,\"fieldName\":\"departmentNo\",\"displayName\":\"系所号\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":17},{\"settingId\":\"5815078f3e9146f4a66b14c5cb939cab\",\"settingType\":1,\"fieldName\":\"otherSchoolForm\",\"displayName\":\"其他办学形式\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":18},{\"settingId\":\"b58bf19704a145d192e790befd9ac20d\",\"settingType\":1,\"fieldName\":\"majorNo\",\"displayName\":\"专业号\",\"isEnabled\":1,\"isTolerateNull\":1,\"orderNO\":19}]}}";
-		 * JSONObject object=JSON.parseObject(jsString);
-		 * System.out.println(object.get("grade")==null); System.out.println(new
-		 * a().aBoolean);
-		 */
-	}
-
-	static class a {
-		Boolean aBoolean;
 	}
 
 	/**
@@ -107,11 +78,19 @@ public class Test {
 		return value;
 	}
 
-	public static void a() {
-		Iterator iterator=null;
-		while (iterator.hasNext()) {
-			System.out.println(1);
-		}
+	/**
+	 * 测试iterator为null时是否会报错
+	 * @throws Exception
+	 * @作者 邓力萌
+	 * @Tags
+	 * @创建时间 2019年3月5日下午4:08:22
+	 */
+	public static void a() throws Exception {
+		try {
+			int a = 1 / 0;
+		} catch (Exception e) {
+			throw new Exception("dfd");
 		}
 	}
+
 }
